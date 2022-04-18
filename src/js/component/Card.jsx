c174c868e24aa4670495ecf4074a1f91b626756f
JLink-1988 <98064614+JLink-1988@users.noreply.github.com>
2022-04-18 09:48:19 -0400

    Create Card.jsx

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+
+export function Card(props) {
+	return (
+		<div className="mb-4 px-2">
+			<div className="card h-100">
+				<img src={props.image} alt="" />
+				<div className="card-body text-center">
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.text}</p>
+					<div class="card-footer bg-transparent border-dark">
+						<a href="#" className="btn btn-primary">
+							Find Out More!
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
